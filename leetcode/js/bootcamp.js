22ef267df0ca1138e45c3095176077b1eda84c56
賴榆文 <laiyuwen@laiyuwende-MacBook-Pro.local>
2023-02-08 06:01:07 +0800

    practice

--- leetcode/js/bootcamp.js
+++ leetcode/js/bootcamp.js
@@ -82,4 +82,20 @@ function maxChar(str) {
   return charMax;
 }
 
+// === 2/8 Fizz buzz ===
+function fizzBuzz(i) {
+  for (let i = 1; i <= n; i++ ) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    }else if (i % 3 === 0) {
+      console.log("fizz") 
+    }else if (i % 5 ===0) {
+      console.log("buzz")
+    }else{
+      console.log(i)
+    }
+  }
+}
+
+// === 2/8 Fizz buzz ===
 
